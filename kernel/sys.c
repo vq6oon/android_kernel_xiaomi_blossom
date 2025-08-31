609acb27783399e7e83a19196b5e649a80ca7d8b
AttentionSeekr <90097027+rsuntk@users.noreply.github.com>
2025-08-31 09:17:47 +0700

    BACKPORT: KernelSU: Implement SuspiciousFS (SusFS) v1.5.9@8acbd107
    * Backported by @rsuntk
    * Removed SusFS feature: Sus_su
    
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>
    Co-authored-by: simonpunk <simonpunk2016@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1262,12 +1262,31 @@ static int override_version(struct new_utsname __user *name)
 #endif
 }
 
+#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
+extern void susfs_spoof_uname(struct new_utsname* tmp);
+#endif
+
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
+#ifndef CONFIG_FAKE_UNAME_NONE
+	if (current_uid().val == 0) {
+		if (!strncmp(current->comm, "bpfloader", 9) ||
+			!strncmp(current->comm, "netbpfload", 10) ||
+			!strncmp(current->comm, "netd", 4) ||
+			!strncmp(current->comm, "uprobestats", 11)) {
+			strcpy(tmp.release, FAKE_UNAME);
+			pr_info("fake uname: %s/%d release=%s\n",
+				current->comm, current->pid, tmp.release);
+		}
+	}
+#endif
+#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
+	susfs_spoof_uname(&tmp);
+#endif
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
